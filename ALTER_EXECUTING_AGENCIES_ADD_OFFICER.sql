3079f33c32fae1b4d529f99583e7679586474657
leela <ls6998020@gmail.com>
2025-11-29 16:38:03 +0530

    state admins

--- ALTER_EXECUTING_AGENCIES_ADD_OFFICER.sql
+++
@@ -1,7 +0,0 @@
--- Add agency_officer column to executing_agencies table
-
-ALTER TABLE executing_agencies 
-ADD COLUMN IF NOT EXISTS agency_officer TEXT;
-
--- Comment on column
-COMMENT ON COLUMN executing_agencies.agency_officer IS 'Name of the officer in charge of the agency';
